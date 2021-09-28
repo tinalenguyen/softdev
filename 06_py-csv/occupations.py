8fb60bd4f1d8bd88549c8aba279974f87aa8ca12
Tina Nguyen <tinanguyen@Tina.local>
2021-09-28 14:11:17 -0400

    added annotations

--- 06_py-csv/occupations.py
+++ 06_py-csv/occupations.py
@@ -1,12 +1,13 @@
 """
-Pasta Noodles: Christopher Liu, Tina Nguyen, Tami Takada
+Pasta Noodles: Christopher Liu, Tina Nguyen, Tami Takeda
 SoftDev
 K06 -- Weighted Random Occupation Picker
-2021-09-27
+2021-09-28
 """
 
 import csv
 import random
+
 
 def read_occupations() -> dict:
     """Reads a CSV file containing job classes and percentages and returns a
@@ -14,17 +15,19 @@ def read_occupations() -> dict:
     Ignores the first header line and adds an 'Other' category for percentages
     outside of the 'Total'."""
 
+    OCCUPATIONS_FILE = "06_py-csv/occupations.csv"
+
     occupations = {}
-    with open("occupations.csv", newline="") as csvfile:
+    with open(OCCUPATIONS_FILE, newline="") as csvfile:
         reader = csv.reader(csvfile)
 
         # We ignore the first header line with the column titles.
         next(reader)
 
         for index, row in enumerate(reader):
-            job_class = row[0]
-            percentage = row[1]
-            occupations[job_class] = float(percentage)
+            job_class = row[0] # added to dict as keys
+            percentage = row[1] # added to dict as values
+            occupations[job_class] = float(percentage) # float for decimals
 
     # We mark everything not in the occupations list as "Other".
     total_percentage = occupations["Total"]
@@ -33,19 +36,22 @@ def read_occupations() -> dict:
 
     return occupations
 
+
 def choose_occupation(occupations: dict) -> str:
     """Picks an occupation randomly using the percentage weights in the given
     occupations dictionary."""
 
-    job_classes = occupations.keys()
-    percentages = occupations.values()
+    job_classes = list(occupations.keys()) # turns keys into a list for use in random.choice
+    percentages = list(occupations.values()) # turns values from dict into a list for use in random.choice
 
-    choice = random.choices(job_classes, weights=percentages)
+    choice = random.choices(job_classes, weights=percentages)[0] # random.choices picks a random job with weighted percentages and returns one value
     return choice
 
+
 def main():
-    occupations = read_occupations()
+    occupations = read_occupations() 
     print(choose_occupation(occupations))
+
 
 if __name__ == "__main__":
     main()
