543e924f53fe52aba80cf2b8e5b773638a0daf41
Tina Nguyen <tinanguyen@Tina.local>
2021-10-19 13:16:42 -0400

    fixed the sessions and the logout button

--- 15_session/app.py
+++ 15_session/app.py
@@ -6,11 +6,18 @@
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+         #
+
+from random import *
+import os
 
 #the conventional way:
 #from flask import Flask, render_template, request
 
+from flask import session
 app = Flask(__name__)    #create Flask object
+app.secret_key = os.urandom(32)
+
 
 
 '''
@@ -30,7 +37,9 @@ def disp_loginpage():
     # print(request.args['username'])
     # print("***DIAG: request.args['password']  ***")
     # print(request.args['password'])
-    return render_template( 'login.html' )
+     if ("username" != None):
+        return render_template( 'response.html', username=session.get("username"))
+     return render_template( 'login.html' )
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
@@ -52,6 +61,7 @@ def authenticate():
 
     myuser="settingthebar"
     mypass="intertoobz"
+
     #authenicate will go to the response page which will use response.html
     #we are retrieving entered username and password here
     username=request.args['username']
@@ -63,15 +73,23 @@ def authenticate():
     #     return render_template( 'response.html', username=request.args['username'], password=request.args['password'])  #response to a form submission
     # except if ((username!=myuser or password!=mypass)):
     #     return render_template( 'login.html' )
+    #app.config['secret_key'] = os.urandom(32)
     if (username==myuser and password==mypass):
-        return render_template( 'response.html', username=username)
+        session["username"] = username
+        return render_template( 'response.html', username = session["username"])
     elif (username=="" or password==""):
         return render_template('login.html', error="Cannot submit blank username or password")
     elif (username!=myuser):
         return render_template('login.html', error="Incorrect username")
     elif (password!=mypass):
-        return render_template('login.html', error="Incorrect passWORD")
+        return render_template('login.html', error="Incorrect password")
 
+@app.route("/logout")
+def logout():
+    #if "username" in session:
+    session["username"] = None
+    session.pop("username", None)
+    return render_template('login.html')
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
